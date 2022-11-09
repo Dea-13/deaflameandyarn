4be1ed324c21b8e3ecae965c85f1692b23ce571c
amddogy <amddogi@gmail.com>
2022-11-09 16:54:34 +0200

    add dev url

--- src/environments/environment.web.ts
+++ src/environments/environment.web.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: false,
   environment: 'WEB',
   hmr: false,
-  apiUrl: 'http://192.168.8.84:91/api/',
+  // apiUrl: 'http://192.168.8.84:91/api/',
+  apiUrl: 'http://185.177.116.190:8003/api/',
   apiReportServer: 'http://192.168.8.240/ReportServer/'
 };
