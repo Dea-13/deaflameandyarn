89c9421ceaf860ee71affb8e19d8c4f5fd7a1235
Dea-13 <72913306+Dea-13@users.noreply.github.com>
2024-04-23 08:59:33 +0300

    upd

--- src/app/app-config.ts
+++ src/app/app-config.ts
@@ -18,8 +18,8 @@ import { CoreConfig } from "./@core/types";
 // prettier-ignore
 export const coreConfig: CoreConfig = {
   app: {
-    appName     : 'DMS - Alcomet',                                        // App Name
-    appTitle    : 'DMS - Alcomet', // App Title
+    appName     : 'DMS',                                        // App Name
+    appTitle    : 'DMS', // App Title
     appLogoImage: 'assets/images/logo/logo.svg',                  // App Logo
     appLanguage : 'en',                                           // App Default Language (en, fr, de, pt etc..)
   },
