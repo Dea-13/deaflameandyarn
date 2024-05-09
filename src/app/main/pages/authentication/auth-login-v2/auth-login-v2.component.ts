98b0ff5069df51d9d2d3640031337dfd0d89c554
Dea-13 <72913306+Dea-13@users.noreply.github.com>
2024-05-09 14:42:10 +0300

    upd

--- src/app/main/pages/authentication/auth-login-v2/auth-login-v2.component.ts
+++ src/app/main/pages/authentication/auth-login-v2/auth-login-v2.component.ts
@@ -40,6 +40,7 @@ export class AuthLoginV2Component implements OnInit {
 
   @ViewChild('myDivRed', { static: false }) myDivRed: ElementRef<HTMLElement>;
   cookieValue: string;
+  rememberMe: any;
 
   /**
    * Constructor
@@ -141,6 +142,10 @@ export class AuthLoginV2Component implements OnInit {
       if (config) {
         console.log('config', config);
         this.cookieService.set('userName', config.userName);
+        if(this.rememberMe) {
+          localStorage.setItem('userName', config.userName);
+          localStorage.setItem('password', this.loginFormUser.controls.password.value);
+        }
         localStorage.setItem('_currentUser', JSON.stringify(config));
         //redirect to home page
         setTimeout(() => {
@@ -174,10 +179,9 @@ export class AuthLoginV2Component implements OnInit {
    * On init
    */
   ngOnInit(): void {
-    console.log('cookie', this.cookieService.get('userName'))
     this.loginFormUser = this._formBuilder.group({
-      email: [this.cookieService.get('userName') ? this.cookieService.get('userName') : '', [Validators.required]],
-      password: ['', Validators.required]
+      email: [localStorage.getItem('userName') ? localStorage.getItem('userName') : '', [Validators.required]],
+      password: [localStorage.getItem('password') ? localStorage.getItem('password') : '', Validators.required]
     });
 
     this.loginFormCard = this._formBuilder.group({
@@ -193,6 +197,10 @@ export class AuthLoginV2Component implements OnInit {
     });
 
     // this.closeTime()
+  }
+
+  remember(event) {
+    this.rememberMe = event.target.checked;
   }
 
   changeForm(value){
